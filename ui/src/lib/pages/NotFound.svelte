76747d0a31bdb44472d7576410960ecbfd164e29
Naufal Altaf <me@altaf.xyz>
2026-09-06 13:44:20 +0700

    Fix 404 button to name its actual destination

--- ui/src/lib/pages/NotFound.svelte
+++ ui/src/lib/pages/NotFound.svelte
@@ -7,5 +7,5 @@
   <p class="mt-2 mb-[18px] text-tertiary">
     The requested route <code class="font-mono text-xs">{path}</code> does not exist.
   </p>
-  <a class="btn btn-primary" href="/">Back to providers</a>
+  <a class="btn btn-primary" href="/">Back to Connections</a>
 </div>
